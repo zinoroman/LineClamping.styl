a1b80df68a8c3538c09a00631f160f0269051b48
Zino <zino.roman95@gmail.com>
2017-01-12 10:39:50 +0200

    Add posibility to import library into stylus

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = (options) => {
+    const implicit = (options && options.implicit == false) ? false : true;
+
+    return (style) => {
+        style.include(__dirname);
+        
+        if (implicit) {
+            style.import('line-clamping'); 
+        }
+    }
+}
